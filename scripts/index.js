15d7e60db90e88dd5793b2d3590a650226f9b89c
malsebasmal <antoniomaldonadoestudio@gmail.com>
2024-03-12 14:23:13 -0500

    fix: index javascript - Arreglando los métodos conforme la clase y la solución del profesor.

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,30 @@
 class Activity{
-    #idGenerator = function(){
-        return Math.floor(Math.random() * (999 - 100 + 100) + 1)
-        //*En algun momento se repetira el número por probabilidad
-        //*Podemos crear un array donde almacenemos los números que salgan no se repitan, haciendo alguna especie de filtrado o comprobación.
-    }
-    #id
-
-    constructor(title, description, imgUrl){
-        this.#id = this.#idGenerator()
-        this.title = title;
-        this.description = description;
-        this.imgUrl = imgUrl;
-    }
-    get id(){
-        return this.#id
-    }
+		constructor(id, title, description, imgUrl){
+				this.id = id;
+				this.title = title;
+				this.description = description;
+				this.imgUrl = imgUrl;
+		}
 }
 
 
 class Repository{
-    constructor(){
-        this.activities = [];
-    }
+		constructor(){
+				this.activities = [];
+				this.id = 0
+		}
 
-    getAllActivities = () => {
-        return this.activities
-    }
+		getAllActivities = () => {
+				return this.activities
+		}
 
-    createActivity = (...instanciaActivity) => {
-        this.activities.push(instanciaActivity)
-    }
+		createActivity = (title, description, imgUrl) => {
+				const id = this.id++
+				const instanciaActivity = new Activity(id, title, description, imgUrl)
+				return this.activities.push(instanciaActivity)
+		}
 
-    deleteActivity = (...instanciaActivity) => {
-        instanciaActivity.id 
+		deleteActivity = (idInstancia) => {
+      this.activities = this.activities.filter(actividad => actividad.id !== idInstancia);
     }
 }
-
-const correr = new Activity("Correr", "Yo corro", "CorroImg")
-
-const repo1 = new Repository()
