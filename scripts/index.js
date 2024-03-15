920aa7f2d7479f7f1efb357fab8486e5e106a418
malsebasmal <antoniomaldonadoestudio@gmail.com>
2024-03-15 18:35:44 -0500

    fix-add: index javascript - Completada la funcionalidad de agregar tarjetas

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,6 @@ class Activity{
 				this.imgUrl = imgUrl;
 		}
 }
-
-
 class Repository{
 		constructor(){
 				this.activities = [];
@@ -28,3 +26,67 @@ class Repository{
       this.activities = this.activities.filter(actividad => actividad.id !== idInstancia);
     }
 }
+let repositoryPrincipal = new Repository()
+
+function createActivityTarget(obj){
+	const {id, title, description, imgUrl} = obj;
+
+	let divTarget = document.createElement("div")
+	divTarget.classList.add("target")
+
+	let h3Title = document.createElement("h3");
+		h3Title.textContent = title;
+	let pDescription = document.createElement("p");
+		pDescription.textContent = description
+	let imgTarget = document.createElement("img");
+		imgTarget.src = imgUrl
+
+	divTarget.append(h3Title, pDescription, imgTarget)
+	
+	return divTarget
+}
+
+function activityHTML(){
+	let sectionForTargets = document.querySelector(".section__main--actividadesAgregadas");
+		sectionForTargets.innerHTML = "";
+	
+	let listCompleteActivities = repositoryPrincipal.getAllActivities()
+	let activitiesConvertHTML = listCompleteActivities.map(objActivityIndex => createActivityTarget(objActivityIndex)) //! No estoy seguro del parámetro que estoy pasando.
+	activitiesConvertHTML.forEach(activityIndex => sectionForTargets.append(activityIndex))
+
+	//?return sectionForTargets //No se si se retorna esto
+}
+
+function buttonConnection() {
+	let inputNameActivity = document.querySelector(".form__input--text-actividades");
+	let inputDescriptionActivity = document.querySelector(".form__input-actividades");
+	let inputUrlActivity = document.querySelector(".form__input--url-actividades")
+			const inputNameValue = inputNameActivity.value;
+			const inputDescriptionValue = inputDescriptionActivity.value;
+			const inputUrlValue = inputUrlActivity.value;
+
+	if (inputNameValue === ""){
+		alert("Por favor, coloca un título a tu actividad")
+			return false
+	} else if (inputDescriptionValue === ""){
+		alert("Por favor, coloca una descripción a tu actividad")
+		return false
+	} else if (inputUrlValue === ""){
+		alert("Por favor, coloca una imagen a tu actividad")
+		return false
+	} else if (!inputUrlActivity.checkValidity()){
+		alert("Por favor, coloca una URL válida a tu actividad")
+		return false
+	} else{
+		repositoryPrincipal.createActivity(inputNameValue, inputDescriptionValue, inputUrlValue);
+		activityHTML()
+		inputNameActivity.value = ""
+		inputDescriptionActivity.value = ""
+		inputUrlActivity.value = ""
+		console.log(inputUrlValue)
+		return true
+	}
+}
+
+let buttonAction = document.querySelector(".form__input--button-actividades");
+buttonAction.addEventListener("click", buttonConnection)
